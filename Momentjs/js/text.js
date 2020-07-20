0c4717ab649998327705bf04c190de35f431a587
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-07-20 13:12:19 +0200

    eserciziocon momentjs  quasi completo

--- Momentjs/js/text.js
+++ Momentjs/js/text.js
@@ -1,32 +1,76 @@
 //Creare un calendario del 2018, inserire poi le festività 
 
 $(document).ready(function(){
-    //var momento= moment().add(1, "day").format("YYYY-MM-DD");
-    //console.log(momento);
-    var giorniMese =[];
-    for(var x = 1; x <= 12; x++){
-        var numeroGiorni = moment("2018-" + x + "", "YYYY-MM").daysInMonth(); //Così ottengo il numero dei giorni di ogni mese dell'anno
-        giorniMese.push(numeroGiorni);
-    } 
+    
+    var numeroMese = 1;
 
-    for(var y = 1; y <= giorniMese[0]; y++){
-        var gennaio = moment("2018-01-" + y + "").format("dddd YYYY-MM-DD"); // Così ottengo ogni signolo giorno del mese di gennaio con relatvi giorni
-        console.log(gennaio);
-    } 
+    function mese(numeroMese){
+        var scrittaMese = moment(numeroMese, "M").format("MMMM");
+        $("#month").text(scrittaMese);
 
-    $.ajax({
-        url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-        success: function(success){
-            var festività = success.response;
-            console.log(festività) // Così ottengo i giorni festivi di gennaio
-            for(var a=0; a<festività.length; a++){
-                console.log(festività[a].date); //Così ottengo la data singola della festività
+
+        var giorniMese = moment("2018-" + numeroMese + "", "YYYY-MM").daysInMonth(); //Così ottengo il numero dei giorni di gennaio
+        console.log(numeroMese);
+
+
+        var dayBox = $("#template-calendar").html(); // Queste due righe servono a copiare il template dei box con dentro i giorni del mese
+        var boxCompile = Handlebars.compile(dayBox); //
+
+        var giornoSettimana = moment("2018-" + numeroMese + "-01", "YYYY-MM-DD").format("e ddd");
+        console.log(giornoSettimana);
+
+        for(var y = 1; y <= giorniMese; y++){
+            var meseCorrente = moment("2018-" + numeroMese + "-" + y + "").format("YYYY-MM-DD"); // Così ottengo ogni signolo giorno del mese di gennaio con relatvi giorni
+            console.log(meseCorrente);
+            var ogettoGiorni = { // Oggetto che contiene le cose da inserire nei box del calendario
+                giorno: y,
+                date : meseCorrente,
             }
-        },
-        error: function(error){
-            console.log(error);
-        }
+            var boxAppend= boxCompile(ogettoGiorni); 
+            $("#contenitor-box").append(boxAppend); // Così stampo il template con i giorni su html
+        } 
+        $.ajax({
+            url: "https://flynn.boolean.careers/exercises/api/holidays",
+            data: {
+                year: 2018,
+                month: numeroMese - 1
+            },
+            success: function(success){
+                var festività = success.response; // Così ottengo i giorni festivi di gennaio
+                for(var a=0; a<festività.length; a++){ //Così ottengo la data singola della festività
+                    console.log(festività[a].date);
+                    var giornoSelezionato = $(".box-day[data-date='"+ festività[a].date +"']");
+                    /*$(".box-day").each(function(){
+                        var box = $(this).attr("data-date");
+                        console.log(box);
+                        if(box == festività[a].date){ //Se il data date è uguale alla festività, allora coloro il box di rosso
+                            $(this).addClass("red"); 
+                        }
+                    })*/
+                    giornoSelezionato.addClass("red");
+                }
+                
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+    }
+    mese(numeroMese);
+    
+
+    $("#next").click(function(){
+        numeroMese++;
+        $(".box-day").remove();
+        mese(numeroMese);
     })
+
+    $("#back").click(function(){
+        numeroMese--;
+        $(".box-day").remove();
+        mese(numeroMese);
+    })
+
 
 
 })
